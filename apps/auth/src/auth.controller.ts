04ba735a8e79f9d894a99eff747ce9b263ca2c7a
Kaushal Panchal <kaushal.panchal@bacancy.com>
2025-07-22 20:45:07 +0530

    changed the josn file location

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -10,6 +10,7 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(
